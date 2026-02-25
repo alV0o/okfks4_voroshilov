347094c190f26638b052e28f5fd480e94e1b1056
boryaxray <alek7ey2003@gmail.com>
2026-02-25 14:52:54 +0500

    first commit classes

---
+++ okfks4_voroshilov/Shape.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace okfks4_voroshilov
+{
+    internal class Shape
+    {
+    }
+}
---
+++ okfks4_voroshilov/Triangle.cs
@@ -0,0 +1,7 @@
+﻿namespace okfks4_voroshilov
+{
+    public class Class1
+    {
+
+    }
+}
